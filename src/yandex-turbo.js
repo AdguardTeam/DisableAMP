7be0ceb233383120930e9d25a43a09db1020e2d1
tvinzz <o.labudko@adguard.com>
2021-07-06 03:11:56 +0300

    changed yandex turbo pages redirect method

--- src/yandex-turbo.js
+++ src/yandex-turbo.js
@@ -1,53 +1,20 @@
 /**
- * Hides Turbo page icon
- * @param {object} turboLink
- */
-const hideTurboIcon = (turboLink) => {
-    if (turboLink.querySelector('.text-with-icon')) {
-        turboLink.style.position = 'absolute';
-        turboLink.style.left = '-99999px';
-    }
-};
-
-/**
- * Disables Yandex Turbo pages
- */
-const disableTurbo = () => {
-    const turboLinks = document.querySelectorAll('a[href^="https://yandex.ru/turbo/"]');
-    [...turboLinks].forEach((link) => {
-        const originalUrl = link.href
-            .replace('yandex.ru/turbo/', '')
-            .replace('/s/', '/');
-
-        link.setAttribute('href', originalUrl);
-
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
-            event.stopPropagation();
-            document.location.href = originalUrl;
-        }, true);
-
-        hideTurboIcon(link);
-    });
-};
-
-/**
  * Redirects from Yandex Turbo page to normal version
  */
 const redirectTurboPages = () => {
-    const originalUrl = document.location.href
-        .split('yandex.ru/turbo/')
-        .pop()
-        .replace('/s/', '/');
+    const postMessage = document.querySelector('script[data-name="post-message"][data-message]');
+    if (!postMessage) {
+        return;
+    }
+    const dataMessage = postMessage.getAttribute('data-message');
+    const dataJson = JSON.parse(dataMessage);
 
-    const protocol = document.location.protocol;
-
-    if (originalUrl && protocol) {
-        document.location.href = `${protocol}//${originalUrl}`;
+    if (dataJson && dataJson.originalUrl) {
+        document.location.href = dataJson.originalUrl;
     }
 };
 
 export {
-    disableTurbo,
+    // eslint-disable-next-line import/prefer-default-export
     redirectTurboPages,
 };
