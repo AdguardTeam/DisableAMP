a2cc0dca1324bb681fedb83a36edf68c44ae6ee3
tvinzz <o.labudko@adguard.com>
2021-10-22 01:00:05 +0300

    added terrnews.com to exclusions

--- src/exclusions.js
+++ src/exclusions.js
@@ -1,6 +1,8 @@
 const exclusions = [
     // automatic redirect to amp in .htaccess
     'https://www.orfonline.org/*',
+    // automatic redirect to yandex turbo in .htaccess
+    'https://yandex.ru/turbo/s/terrnews.com/*',
 ];
 
 module.exports = exclusions;
