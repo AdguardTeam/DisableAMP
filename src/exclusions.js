2106221278fcb123b1e8570e1ef8aa2608352e07
Stanislav Atroschenko <s.atroschenko@adguard.com>
2022-01-13 14:10:13 +0300

    AG-11686 infinite redirects on tehnichka.pro
    Merge in EXTENSIONS/disable-amp from fix/AG-11686 to master
    
    Squashed commit of the following:
    
    commit 63904f0a79a1a28531148e77938e2db7b98290a4
    Author: Stanislav A <s.atroschenko@adguard.com>
    Date:   Tue Jan 11 16:29:56 2022 +0300
    
        add tehnichka.pro to exclusions

--- src/exclusions.js
+++ src/exclusions.js
@@ -1,6 +1,7 @@
 const exclusions = [
     // automatic redirect to amp in .htaccess
     'https://www.orfonline.org/*',
+    'https://tehnichka.pro/*',
     // automatic redirect to yandex turbo in .htaccess
     'https://yandex.ru/turbo/s/terrnews.com/*',
 ];
